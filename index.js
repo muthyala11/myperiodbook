d5f4433c36007ef5445bd8337179e06e94cb3944
kavya sri <muthyalakavyasri@gmail.com>
2023-03-24 10:57:17 -0400

    singup

--- index.js
+++ index.js
@@ -33,6 +33,36 @@ app.post('/login', async (req, res) => {
   }
 });
 
+// signup route with express
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body)
+
+  try {
+    // connecting to mongo
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+
+    // check if user already exists
+    const existingUser = await db.collection('users').findOne({ email });
+    if (existingUser) {
+      res.status(409).send('User already exists');
+      return;
+    }
+
+    // create new user
+    const newUser = { email, password };
+    const result = await db.collection('users').insertOne(newUser);
+    console.log(`A new user was created with the _id: ${result.insertedId}`);
+    res.send('Signup successful');
+
+    client.close();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
